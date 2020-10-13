51277eac7e664462632c6d0e25dd53387c60ee91
Joona Mellin <joona.mellin@live.fi>
2020-10-13 21:37:07 +0300

    Trainings GET + AgGrid
    Added GET method for filling up the table, and AG Grid component for rendering the table

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Home from './components/pages/Home';
 import Customers from './components/pages/Customers';
+import Trainings from './components/pages/Trainings';
 
 function App() {
   return (
     <div className="App">
       <Customers />
+      <Trainings />
     </div>
   );
 }
--- src/components/pages/Trainings.js
+++ src/components/pages/Trainings.js
@@ -3,17 +3,17 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
-export default function Customers() {
+export default function Trainings() {
 
     // Let's render the fetch once per render
     useEffect(() => {
-        getCustomers();
+        getTrainings();
     }, []);
 
     // Let's first define the parameters we'll be using
 
     // We're going to need an array of Customers
-    const [ customers, setCustomers ] = useState([]);
+    const [ trainings, setTrainings ] = useState([]);
     // We'll use a message....
     const [ message, setMessage ] = useState('');
     // together with a Snackbar modal to give feedback to the user
@@ -21,36 +21,44 @@ export default function Customers() {
 
 
     //  GET FUNCTION - Reads all Customers from the server, and saves them into Customers object array
-    const getCustomers = () => {
-        fetch('https://customerrest.herokuapp.com/api/customers')
+    const getTrainings = () => {
+        fetch('https://customerrest.herokuapp.com/gettrainings')
         .then(response => response.json())
-        .then(data => setCustomers(data.content))
+        .then(data => setTrainings(data))
     }
 
 
 
     // AG GRID - Ag Grid column definitions
     const columns = [
-        { headerName: "First name", field: 'firstname', sortable: true },
-        { headerName: "Last name", field: 'lastname', sortable: true },
-        { headerName: "Street Address", field: 'streetaddress', sortable: true },
-        { headerName: "Postcode", field: 'postcode', sortable: true },
-        { headerName: "City", field: 'city', sortable: true },
-        { headerName: "Email", field: 'email', sortable: true },
+        { headerName: "Activity", field: 'activity', sortable: true, filter: true },
+        { headerName: "Date", field: 'date', sortable: true, filter: true },
+        { headerName: "Duration", field: 'duration', sortable: true, filter: true },
+        { 
+            headerName: "Customer",  
+            sortable: 'true',
+            filter: true,
+            cellRenderer: function(params) {
+                return (
+                    params.data.customer.firstname + " " + params.data.customer.lastname
+                )
+            }
+        },
+
     ]
 
     return(
         <div>
             <div className="jumbotron jumbotron-fluid">
                 <div className="container">
-                    <h1 className="display-4">Customers</h1>
-                    <p className="lead">Display all Customers</p>
+                    <h1 className="display-4">Workouts</h1>
+                    <p className="lead">Display all Workouts</p>
                 </div>
             </div>
             <div className="ag-theme-material" style={{height: '700px', width: '80%', margin: 'auto'}}>
                 <AgGridReact
                     columnDefs={columns}
-                    rowData={customers}
+                    rowData={trainings}
                 />
             </div>
         </div>
