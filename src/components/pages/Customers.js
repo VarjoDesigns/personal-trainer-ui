294d536222c2c8662e15ccdad17946a31153ae1f
Joona Mellin <joona.mellin@live.fi>
2020-10-13 19:49:41 +0300

    Customers LIST (React Table)
    Listing all customers works now with a React Table component. Might switch to AG Grid though.

--- src/components/pages/Customers.js
+++ src/components/pages/Customers.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 export default function Customers() {
 
@@ -22,11 +24,53 @@ export default function Customers() {
         fetch('https://customerrest.herokuapp.com/api/customers')
         .then(response => response.json())
         .then(data => setCustomers(data.content))
-    } 
+    }
+
+    console.log('Customers loaded' + customers)
+
+
+
+    // REACT TABLES
+    const columns = [
+        {
+            Header: 'First name',
+            accessor: 'firstname'
+        },
+        {
+            Header: 'Last name',
+            accessor: 'lastname'
+        },
+        {
+            Header: 'Street address',
+            accessor: 'streetaddress'
+        },
+        {
+            Header: 'Postcode',
+            accessor: 'postcode'
+        },
+        {
+            Header: 'City',
+            accessor: 'city'
+        },
+        {
+            Header: 'Email',
+            accessor: 'email'
+        },
+        {
+            Header: 'Phone',
+            accessor: 'phone'
+        }
+    ]
 
     return(
         <div>
-            
+            <h1>Customers</h1>
+            <ReactTable
+                filterable={true}
+                data={customers}
+                columns={columns}
+                defaultPageSize={50}
+            />
         </div>
     )
 }
